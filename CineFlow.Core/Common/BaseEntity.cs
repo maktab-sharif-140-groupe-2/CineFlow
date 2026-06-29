65ef444ad1b35fb926ca96553bc22ee62b5ac902
Mahdi <Mahdi@DESKTOP-ATMOOIR>
2026-06-29 18:32:17 +0430

    Add BaseEntity

---
+++ CineFlow.Core/Common/BaseEntity.cs
@@ -0,0 +1,27 @@
+﻿namespace CineFlow.Core.Common;
+
+public abstract class BaseEntity
+{
+
+    public Guid Id { get; private set; }
+
+    public bool IsDelete { get; set; }
+
+    public DateTime? DeleteAt { get; set; }
+
+    public DateTime? ModifiedAt { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    protected abstract void Validate();
+
+    public void Delete()
+    {
+        IsDelete = true;
+        DeleteAt = DateTime.UtcNow;
+        ModifiedAt = DateTime.UtcNow;
+
+    }
+
+
+}

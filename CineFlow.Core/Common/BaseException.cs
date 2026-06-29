377ccd170d13f1ed438543b4a1aab885199e7da2
Hemen Karimi <hemnkarimi@gmail.com>
2026-06-29 18:39:53 +0330

    Add BaseException And DomainException

---
+++ CineFlow.Core/Common/BaseException.cs
@@ -0,0 +1,11 @@
+﻿namespace CineFlow.Core.Common;
+
+public abstract class BaseException : Exception
+{
+    public BaseException(string message, string code, Exception? innerException = null) : base(message, innerException)
+    {
+        Code = code;
+    }
+
+    public string Code { get; }
+}
---
+++ CineFlow.Core/Exceptions/DomainException.cs
@@ -0,0 +1,11 @@
+﻿using CineFlow.Core.Common;
+
+namespace CineFlow.Core.Exceptions;
+
+public class DomainException : BaseException
+{
+    public DomainException(string message, string code, Exception? innerException = null) : base(message, $"DomainException_{code}", innerException)
+    {
+    }
+
+}
